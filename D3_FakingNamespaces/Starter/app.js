d4262b08d11f49f240e7303928bc8c6bb4310b3a
Kyle Desktop <kylespresser@gmail.com>
2019-01-09 14:51:55 -0500

    lecture 33

--- D3_FakingNamespaces/Starter/app.js
+++ D3_FakingNamespaces/Starter/app.js
@@ -1,4 +1,12 @@
 var greet = 'Hello!';
-var greet = 'Hola!'; 
+var greet = 'Hola!';
 
 console.log(greet);
+
+var english = {};
+var spanish = {};
+
+english.greet = 'Hello';
+spanish.greet = 'Hola';
+
+console.log(english);
