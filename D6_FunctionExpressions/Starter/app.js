d289e5b19ee1272f9eb39f292214e85b25d3a115
Kyle Desktop <kylespresser@gmail.com>
2019-01-09 15:23:27 -0500

    lecture 35

---
+++ D6_FunctionExpressions/Starter/app.js
@@ -0,0 +1,23 @@
+// var a;
+
+// if (a === 3) {
+
+// }
+
+greet();
+
+function greet() {
+  console.log('hi');
+}
+
+var anonymousGreet = function() {
+  console.log('hi');
+};
+
+anonymousGreet();
+
+function log(a) {
+  console.log(a);
+}
+
+log(3);
