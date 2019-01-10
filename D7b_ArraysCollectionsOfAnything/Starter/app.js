ab0899859f5fcde98e0a19d6d29e66ce925ea4c3
Kyle Desktop <kylespresser@gmail.com>
2019-01-10 09:43:33 -0500

    lecture 38

---
+++ D7b_ArraysCollectionsOfAnything/Starter/app.js
@@ -0,0 +1,17 @@
+var arr = [
+  1,
+  false,
+  {
+    name: 'Kyle',
+    address: 'Under the bridge'
+  },
+  function(name) {
+    var greeting = 'hello ';
+    console.log(greeting + name);
+  },
+  'hello'
+];
+
+console.log(arr);
+
+arr[3](arr[2].name);
