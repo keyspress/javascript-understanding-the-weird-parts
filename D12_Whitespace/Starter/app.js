dcc3ecf2e845751f6455f2c10d75ac3122fa4d1c
Kyle Desktop <kylespresser@gmail.com>
2019-01-10 10:26:40 -0500

    lecture 43

---
+++ D12_Whitespace/Starter/app.js
@@ -0,0 +1,18 @@
+var // first name of the person
+  firstname,
+  // last name of the person
+  lastname,
+  //the language
+  // can be 'en' or 'es'
+  language;
+
+var person = {
+  // the first name
+  firstname: 'Jim',
+
+  // last name
+  // required
+  lastname: 'Jones'
+};
+
+console.log(person);
