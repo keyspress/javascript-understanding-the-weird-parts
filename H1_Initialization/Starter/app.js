97c507c5e5f3ae267b03a6521df326d32e143fb4
Kyle Desktop <kylespresser@gmail.com>
2019-01-11 11:37:16 -0500

    lecture 65

---
+++ H1_Initialization/Starter/app.js
@@ -0,0 +1,15 @@
+var people = [
+  {
+    firstname: 'John',
+    lastame: 'Doe',
+    addresses: ['111 Main St', '222 Second St.']
+  },
+  {
+    firstname: 'Jane',
+    lastname: 'Doe',
+    addresses: ['333 Main St', '444 Fifth St.'],
+    greet: function() {
+      return 'Hello';
+    }
+  }
+];
